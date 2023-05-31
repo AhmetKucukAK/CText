40cf4d0127280ad2fb1ad913038e524b488f5624
AhmetKucukAK <54936799+AhmetKucukAK@users.noreply.github.com>
2023-05-31 12:36:32 +0100

    Update nonFuckedDB.cs

--- nonFuckedDB.cs
+++ nonFuckedDB.cs
@@ -1,23 +1,45 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Finisar.SQLite;
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
 
-namespace learningCsharp
+namespace CTextCurrent
 {
-  public class database
+  public class Config
   {
       public SQLiteConnection sqlite_conn;
       public SQLiteCommand sqlite_cmd;
       public SQLiteDataReader sqlite_datareader;
+    
+      public Config(){}
 
-      public database()
+      public void Connect(string dbName)
       {
-           sqlite_conn = new SQLiteConnection("Data Source=database.db;Version=3;New=True;Compress=True;");
+           sqlite_conn = new SQLiteConnection("Data Source=" + dbName + ".db;Version=3;New=True;Compress=True;"); // TODO: CHECK DIFFERENCES
            sqlite_conn.Open();
            sqlite_cmd = sqlite_conn.CreateCommand();
-       }   
-
+       }
+    
+    public Object[] checkLogin(string Sql_command){
+         var command = sqlite_conn.CreateCommand();
+         command.CommandText = Sql_command;
+      
+         int count = 0;
+         Object[] returnData = new Object[2]  //first element is boolean, second is username
+         using(var reader = command.ExecuteReader()){
+              while (reader.Read()){
+                  returnData[1] = reader.GetString(0);
+                  count = count + 1;
+              }
+          }
+          returnData[0] = (count == 1);
+          return returnData;
+    }
+    
+   public void Execute(string query){
+       var command = sqlite_conn.CreateCommand();
+       command.CommandText = query;
+       command.ExecuteNonQuery();
+   }
+   
   }
 }
